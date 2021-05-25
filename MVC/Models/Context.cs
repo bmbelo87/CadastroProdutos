357bfff90e2d2f1cb55d2ce4b63efd9adcae4b3f
Bruno Silver <bmbelo87@gmail.com>
2021-05-24 21:58:23 -0300

    Criação base

---
+++ MVC/Models/Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class Context : DbContext
+    {
+        public virtual DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=mvccursodbserver.database.windows.net,1433;Initial Catalog = MVC_db; User Id = cursomvc@mvccursodbserver;Password=Silver12");
+        }
+       
+        /*public Context(DbContextOptions<Context> options) : base(options)
+        { }*/
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
